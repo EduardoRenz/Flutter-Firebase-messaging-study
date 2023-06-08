c5b30d0f62ff349eba05efc94616b2273b9bad5f
Eduardo Renz <dudarenz@gmail.com>
2023-06-07 23:45:24 -0300

    setup of messaging

--- app/lib/http/web.dart
+++ app/lib/http/web.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:meetups/models/device.dart';
 import 'package:meetups/models/event.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 const String baseUrl = 'http://192.168.0.176:8081/api';
 
@@ -26,4 +27,14 @@ void sendDevice(Device device) async {
         'modelo': device.model ?? '',
         'marca': device.brand ?? '',
       }));
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString('pushToken', device.token ?? '');
+  await prefs.setBool('tokenSent', false);
+
+  if (response.statusCode != 200) {
+    throw Exception('Falha ao enviar o token');
+  }
+
+  await prefs.setBool('tokenSent', true);
 }
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,10 +1,52 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:meetups/http/web.dart';
+import 'package:meetups/models/device.dart';
 import 'package:meetups/screens/events_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> setPushToken(String? token) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? prefsToken = prefs.getString('pushToken');
+  bool? prefsSent = prefs.getBool('tokenSent');
+
+  if ((token == prefsToken) || (prefsSent == true)) {
+    return;
+  }
+
+  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+  String? brand;
+  String? model;
+
+  if (Platform.isAndroid) {
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    brand = androidInfo.brand;
+    model = androidInfo.model;
+  }
+  if (Platform.isIOS) {
+    IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+    brand = iosInfo.systemName;
+    model = iosInfo.systemVersion;
+  }
+
+  Device device = Device(
+    token: token,
+    model: model,
+    brand: brand,
+  );
+  sendDevice(device);
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  await Firebase.initializeApp();
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  String? token = await messaging.getToken();
+  await setPushToken(token);
   runApp(App());
 }
 
